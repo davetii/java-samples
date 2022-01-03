df113fe923655762280a3a5e1703652d20e28a0d
Dave Turner <davetii@gmail.com>
2022-01-03 18:14:55 -0500

    Added FInd the town judge example

--- src/main/java/com/greatwideweb/samples/conversions/BooleanToChar.java
+++
@@ -1,9 +0,0 @@
-package com.greatwideweb.samples.conversions;
-
-public class BooleanToChar {
-    public static void main(String[] args) {
-        Boolean b = false;
-        char a = (b) ? '0' : '1';
-
-    }
-}
