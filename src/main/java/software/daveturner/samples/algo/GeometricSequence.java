cf8dba809deb945720c4cce9057ff424fd726177
davetii <davetii@gmail.com>
2024-11-26 15:55:36 -0500

    Added Geometric Sequence next value calculator

--- src/main/java/software/daveturner/samples/algo/GeometricSequence.java
+++ src/main/java/software/daveturner/samples/algo/GeometricSequence.java
@@ -6,8 +6,12 @@ public class GeometricSequence {
         return ar[0] * (1 - Math.pow(r, ar.length)) / (1 - r) ;
     }
 
+    public static int next(int[] ar, int r) {
+        return (int) (ar[0] * Math.pow(r, ar.length));
+    }
+
     public static void main(String[] args) {
         int [] s = {3,12,48,192,768};
-        System.out.println(sum(s, 4));
+        System.out.println(next(s, 4));
     }
 }
