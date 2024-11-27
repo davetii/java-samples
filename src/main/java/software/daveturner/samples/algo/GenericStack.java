59c6f80994d1e47af4413ca0ed64d289b961df9a
davetii <davetii@gmail.com>
2024-11-27 09:47:12 -0500

    Implemented a Generic Stack

---
+++ src/main/java/software/daveturner/samples/algo/GenericStack.java
@@ -0,0 +1,27 @@
+package software.daveturner.samples.algo;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
+public class GenericStack<T> {
+    List<T> l = new ArrayList<>();
+
+    public T pop() {
+        if (l.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return l.remove(l.size() -1);
+    }
+
+    public T peek() {
+        if (l.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return l.get(l.size() -1);
+    }
+
+    public void push(T x) {
+        l.add(x);
+    }
+}
---
+++ src/test/java/software/daveturner/samples/algo/GenericStackTest.java
@@ -0,0 +1,21 @@
+package software.daveturner.samples.algo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GenericStackTest {
+
+    @Test
+    public void ensureNumberStackReturnsExpected() {
+        GenericStack<Integer> stack = new GenericStack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Assertions.assertEquals(stack.peek(), 3);
+        Assertions.assertEquals(stack.pop(), 3);
+        Assertions.assertEquals(stack.peek(), 2);
+        Assertions.assertEquals(stack.pop(), 2);
+        Assertions.assertEquals(stack.peek(), 1);
+    }
+
+}
